0cf69786a872b4f22f1259d940480d5cb1571520
Steven Leong <steveleong8@gmail.com>
2019-02-22 14:46:35 -0700

    Updates

--- precache-manifest.73da122480ab99b56fef4382cc0b7921.js
+++ precache-manifest.73da122480ab99b56fef4382cc0b7921.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/MatrixVision/static/js/runtime~main.a2eefbe9.js"
   },
   {
-    "revision": "37e29c57f796c5606bdf",
-    "url": "/MatrixVision/static/js/main.37e29c57.chunk.js"
+    "revision": "37cd790f886f7a090852",
+    "url": "/MatrixVision/static/js/main.37cd790f.chunk.js"
   },
   {
     "revision": "64b35ba1cb96cc7c5a88",
     "url": "/MatrixVision/static/js/1.64b35ba1.chunk.js"
   },
   {
-    "revision": "37e29c57f796c5606bdf",
-    "url": "/MatrixVision/static/css/main.6dd101c5.chunk.css"
+    "revision": "37cd790f886f7a090852",
+    "url": "/MatrixVision/static/css/main.893f6511.chunk.css"
   },
   {
-    "revision": "f41716001c1712764a56e2cca21388b8",
+    "revision": "362343813c0853d1e2d992e47cff6be0",
     "url": "/MatrixVision/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/MatrixVision/precache-manifest.dc3d313a4a4607c2cea3e634305a9e10.js"
+  "/MatrixVision/precache-manifest.73da122480ab99b56fef4382cc0b7921.js"
 );
 
 workbox.clientsClaim();
